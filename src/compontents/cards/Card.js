b58df937e64c1d5015c135ee87f6c5a09b9bb01e
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-22 16:34:51 -0400

    ccard function created

--- src/compontents/cards/Card.js
+++ src/compontents/cards/Card.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 function App(){
     return(
         <div className="wrapper">
-            <h1>Hello</h1>
+            <Card />
+        </div>
+    )
+}
+
+function Card(){
+    return(
+        <div className="card">
+            <div className="card__body">
+                <img src="https://s3.envato.com/files/266452806/11022018-IMG_4933.jpg" />
+                <h2 className="card__title">Test Coin</h2>
+                <p className="card__description">Test Description</p>
+                <button className="card__btn">View Coin</button>
+            </div>
         </div>
     )
 }

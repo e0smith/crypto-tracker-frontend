18f9959c0447fe4917be38ffd8cad21b5c60203b
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-22 12:38:06 -0400

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './compontents/Navbar';
+import Home from './pages';
+import SignUp from './pages/signup';
+import Portfolio from './pages/portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/portfolio' component={Portfolio} />
+        {/* <Route path='/' component={Coin} /> */}
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 

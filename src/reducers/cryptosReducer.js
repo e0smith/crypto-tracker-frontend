e0f93d17518172ae7d6b83ecb1b83063fe245b4c
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-24 14:05:34 -0400

    cryptoReducer modified to accept inital state and action

--- src/reducers/cryptosReducer.js
+++ src/reducers/cryptosReducer.js
@@ -1,3 +1,22 @@
-const cryptosReducer = () => ({ cryptos: []})
+const initialState = {
+    cryptos: []
+}
+
+const cryptosReducer = (state = initialState, action ) => {
+    switch(action.type) {
+        case "ADD_CRYPTOS":
+            return {
+                ...state,
+                cryptos: [...state.cryptos, action.payload]
+            }
+        case "GET_CRYPTOS":
+            return {
+                ...state,
+                cryptos: [...state.cryptos, ...action.payload]
+            }
+        default:
+            return state
+    }
+}
 
 export default cryptosReducer;

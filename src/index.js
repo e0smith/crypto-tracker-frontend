6f31a1339ff0ae22665424d2356661bcd32af702
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-23 21:29:13 -0400

    added reducer + work on store

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 
-const store = createStore(() => {})
+const store = createStore(() => ({cryptos: []}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
-    <provider store={store}>
+    <Provider store={store}>
     <App />
-    </provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

fd87c75a099276452db75832da6c2170c7b07724
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-24 16:08:15 -0400

    unused code comment removed from index.ks

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { applyMiddleware, compose, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import cryptosReducer from './reducers/cryptosReducer';
 import thunk from 'redux-thunk';
 
 const store = createStore(cryptosReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-

658ad795672f0a8f94f2cdba0f6d34a617ba7cf2
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-24 14:04:11 -0400

    Card.js changed to use store, not working yet

--- src/compontents/cards/Card.js
+++ src/compontents/cards/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function Card(props){
+
+function Card(){
+    const { image, name, current_price} = this.props.crypto
     return(
         <div className="card">
             <div className="container">
-                <img src={props.crypto.image}  alt="img"/>
-                <h2 className="card__title">{props.crypto.name}</h2>
-                <p className="card__description">${props.crypto.current_price}</p>
+                <img src={image}  alt="img"/>
+                <h2 className="card__title">{name}</h2>
+                <p className="card__description">${current_price}</p>
                 <button className="card__btn">View Coin</button>
             </div>
         </div>

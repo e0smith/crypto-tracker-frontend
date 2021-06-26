fc975bf3ce80f43740eb25281de2bc3217dc9d6b
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-25 22:25:45 -0400

    route to info page for coins accepts prop argument

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './compontents/Navbar';
 import Home from './compontents/pages';
+import Info from './compontents/pages/coin';
 import SignUp from './compontents/pages/signup';
 import Portfolio from './compontents/pages/portfolio';
 import {connect} from 'react-redux'
 import { getCryptos } from './actions/crypto';
-// import Coin from './compontents/Coin';
 class App extends Component {
   componentDidMount(){
     this.props.getCryptos()
@@ -20,7 +20,11 @@ class App extends Component {
         <Switch>
           <Route path='/home' exact component={Home} />
           <Route path='/portfolio' component={Portfolio} />
-          {/* <Route path='/currency/:id' compontent={Coin} /> */}
+          <Route
+            exact
+            path="/currency/:id"
+            render={(props) => <Info {...props} cryptos={this.props.cryptos} />}
+          />
           {/* <Route path='/' component={Coin} /> */}
           <Route path='/sign-up' component={SignUp} />
         </Switch>

bc622fa5826b7553ca9c81bdef2d770b712762d7
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-26 20:22:56 -0400

    rootReducer replaces cryptoReducer in const store in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import { applyMiddleware, createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
-import cryptosReducer from './reducers/cryptosReducer';
+import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
-
-const store = createStore(cryptosReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+// import portfolioReducer from './reducers/portfolioReducer'
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 ReactDOM.render(
   <React.StrictMode>

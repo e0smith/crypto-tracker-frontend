7adc8575c1b72ca6ba48e67926fedd7697320d25
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-26 20:26:43 -0400

    mapping crypto modified for new version of store, breaks added to spread out rendered text

--- src/compontents/pages/coin.js
+++ src/compontents/pages/coin.js
@@ -1,36 +1,36 @@
 import React from 'react';
 
 const Info = ({ match, cryptos}) => {
-  let crypto = cryptos.find(c => c.id === parseInt(match.params.id))
+  let crypto = cryptos.cryptos.find(c => c.id === parseInt(match.params.id))
   return (
     <div
-      // style={{
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   // alignItems: 'center',
-      //   height: '20vh'
-      // }}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
     >
-      <img src={crypto.image}  alt="img" align="middle"/>
-      <h1 align="center" >{crypto.name}({crypto.symbol})</h1>
-      <h3>Current Price: ${crypto.current_price}</h3>
-      <h3>Market Cap: ${crypto.market_cap}</h3>
-      <h3>24 Hour Market Cap Change: ${crypto.market_cap_change_24h}</h3>
-      <h3>24 Hour Market Cap Change Percentage: {crypto.market_cap_change_percentage_24h}%</h3>
-      <h3>Total Volume: ${crypto.total_volume}</h3>
-      <h3>24 Hour High: ${crypto.high_24h}</h3>
-      <h3>24 Hour Low: ${crypto.low_24h}</h3>
-      <h3>24 Hour Price Change: ${crypto.price_change_24h}</h3>
-      <h3>24 Hour Price Change Percentage: {crypto.price_change_percentage_24h}% </h3>
-      <h3>7 Day Price Change Percentage: {crypto.price_change_percentage_7d}% </h3>
-      <h3>14 Day Price Change Percentage: {crypto.price_change_percentage_14d}% </h3>
-      <h3>30 Day Price Change Percentage: {crypto.price_change_percentage_30d}% </h3>
-      <h3>60 Day Price Change Percentage: {crypto.price_change_percentage_60d}% </h3>
-      <h3>One Year Price Change Percentage: {crypto.price_change_percentage_1y}% </h3>
-      <h3>Total Supply: {crypto.total_supply}</h3>
-      <h3>Circulating Supply: {crypto.circulating_supply}</h3>
-      <h3></h3>
-      <h3></h3>
+      <div>
+        <img src={crypto.image}  alt="img" align="right"/><br /><br /><br /><br /><br />
+        <h1 align="center" >{crypto.name}({crypto.symbol})</h1><br />
+        <h3>Current Price: ${crypto.current_price}</h3><br />
+        <h3>Market Cap: ${crypto.market_cap}</h3><br />
+        <h3>24 Hour Market Cap Change: ${crypto.market_cap_change_24h}</h3><br />
+        <h3>24 Hour Market Cap Change Percentage: {crypto.market_cap_change_percentage_24h}%</h3><br />
+        <h3>Total Volume: ${crypto.total_volume}</h3><br />
+        <h3>24 Hour High: ${crypto.high_24h}</h3><br />
+        <h3>24 Hour Low: ${crypto.low_24h}</h3><br />
+        <h3>24 Hour Price Change: ${crypto.price_change_24h}</h3><br />
+        <h3>24 Hour Price Change Percentage: {crypto.price_change_percentage_24h}% </h3><br />
+        <h3>7 Day Price Change Percentage: {crypto.price_change_percentage_7d}% </h3><br />
+        <h3>14 Day Price Change Percentage: {crypto.price_change_percentage_14d}% </h3><br />
+        <h3>30 Day Price Change Percentage: {crypto.price_change_percentage_30d}% </h3><br />
+        <h3>60 Day Price Change Percentage: {crypto.price_change_percentage_60d}% </h3><br />
+        <h3>One Year Price Change Percentage: {crypto.price_change_percentage_1y}% </h3><br />
+        <h3>Total Supply: {crypto.total_supply}</h3><br />
+        <h3>Circulating Supply: {crypto.circulating_supply}</h3>
+      </div>
     </div>
   );
 };

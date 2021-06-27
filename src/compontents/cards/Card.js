d2d2f56cddfbec3e196af4d473bcc973360dea58
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-26 21:48:53 -0400

    useParams removed from import in Card.js

--- src/compontents/cards/Card.js
+++ src/compontents/cards/Card.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function Card(props){
     const { image, name, current_price, id} = props.crypto
     return(
-        <div className="card">
-            <div className="container">
-                <img src={image}  alt="img"/>
-                <h2 className="card__title">{name}</h2>
-                <p className="card__description">${current_price}</p>
-                <Link to={`currency/${id}`}>View Coin</Link>
-            </div>
+        <div className='card'>
+            <img src={image}  alt="img"/>
+            <h2 className="card__title">{name}</h2>
+            <p className="card__description">${current_price}</p>
+            <Link to={`currency/${id}`}>View Coin</Link>
         </div>
     )
 }

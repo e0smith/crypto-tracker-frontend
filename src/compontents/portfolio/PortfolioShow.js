e6d8a520efb7b90bb81e06992661e5449d462f12
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-28 18:27:27 -0400

    useState testing for portfolio edit

--- src/compontents/portfolio/PortfolioShow.js
+++ src/compontents/portfolio/PortfolioShow.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PortfolioEdit from './PortfolioEdit';
 
 function PortfolioShow(props){
-    const { name, quantity} = props.portfolio
+    const { name, quantity, id} = props.portfolio
+    // const [formBoolean, setFormBoolean] = useState(false)
     // console.log(props.portfolio)
     return(
         <div className='card'>
             <p className="portfolio__info">Name: {name} Quantity: {quantity}</p>
+            {/* <button onClick={() => setFormBoolean(!formBoolean)} >Edit Portfolio</button> */}
+            {/* {formBoolean? <PortfolioEdit portfolio={id}/>: console.log(formBoolean)} */}
         </div>
     )
 }
-
 
 export default PortfolioShow;

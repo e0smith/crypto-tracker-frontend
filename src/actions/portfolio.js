4942d34951f4e3b8fe32c71ee7bee54de7741ead
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-28 18:26:11 -0400

    editPortfolio patch in actions created,

--- src/actions/portfolio.js
+++ src/actions/portfolio.js
@@ -34,3 +34,22 @@ export const getPortfolio = () => {
             })
         }
 }
+
+export const editPortfolio = (data) => {
+    // debugger;
+    return (dispatch) => {
+      fetch(`http://localhost:3000/portfolios/${data.id}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "PATCH",
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((portfolio) => {
+            dispatch({ type: "EDIT_PORTFOLIO", payload: portfolio });
+          }
+        );
+    };
+  };

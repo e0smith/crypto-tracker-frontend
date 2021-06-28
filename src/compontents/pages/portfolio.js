f403b61e26cf5ddfd91cf4c3072994f980f78604
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-28 18:24:26 -0400

    edit portfolio added to portfolio.js pagee, not in use

--- src/compontents/pages/portfolio.js
+++ src/compontents/pages/portfolio.js
@@ -4,18 +4,24 @@ import PortfolioShow from '../portfolio/PortfolioShow';
 import {connect} from 'react-redux';
 
 const Portfolio = (props) => {
+  // const portfolios = props.portfolio.portfolio.map((p) => p)
   return (
     <div>
       <h1>Portfolio</h1><br />
       <PortfolioForm /><br /><br />
+      {/* {portfolios.map((p) => <PortfolioShow portfolio={p} />)} */}
+      {/* <PortfolioEdit /> */}
+      {/* {props.portfolio.portfolio.map((portfolio) => (<PortfolioEdit portfolio={portfolio} />))} */}
       {props.portfolio.portfolio.map((portfolio) => (<PortfolioShow portfolio={portfolio} />))}
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
+  // console.log("state: ", state)
   return{
     portfolio: state.portfolio
   }
+
 }
 export default connect(mapStateToProps)(Portfolio);

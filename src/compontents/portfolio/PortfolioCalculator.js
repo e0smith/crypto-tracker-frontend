9629efb905c5d441b3ab8db9fc3fe6fb9c095e6a
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-29 12:43:27 -0400

    names set to lowercase after being input to solve letter casing  issue in calculator

--- src/compontents/portfolio/PortfolioCalculator.js
+++ src/compontents/portfolio/PortfolioCalculator.js
@@ -1,11 +1,8 @@
 import {connect} from 'react-redux'
 
 const PortfolioCalculator = (props) => {
-    const str = props.portfolio.name
-    const str2 = str.charAt(0).toUpperCase() + str.slice(1)
     let x = "";
-    const calculate = props.cryptos.cryptos.some(item => str2 === (x = item).name) ? "$" + (x.current_price * props.portfolio.quantity) : "Unknown Currency"
-    
+    const calculate = props.cryptos.cryptos.some(item => props.portfolio.name.toLowerCase() === (x = item).name.toLowerCase()) ? "$" + (x.current_price * props.portfolio.quantity) : "Unknown Currency"
     
     return(
         calculate

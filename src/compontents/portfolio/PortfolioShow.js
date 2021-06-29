ab0d8af7808f448c52942655ad38dd2294d5833b
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-29 11:00:48 -0400

    delete and calculate protfolio imported into PortfolioShow

--- src/compontents/portfolio/PortfolioShow.js
+++ src/compontents/portfolio/PortfolioShow.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
-import PortfolioEdit from './PortfolioEdit';
+// import React, { useState }from 'react';
+// import PortfolioEdit from './PortfolioEdit';
+import React from 'react';
+import DeletePortfolio from './PortfolioDelete';
+import PortfolioCalculator from './PortfolioCalculator';
 
 function PortfolioShow(props){
-    const { name, quantity, id} = props.portfolio
+    const { name, quantity} = props.portfolio
+
     // const [formBoolean, setFormBoolean] = useState(false)
     // console.log(props.portfolio)
     return(
         <div className='card'>
             <p className="portfolio__info">Name: {name} Quantity: {quantity}</p>
+            <DeletePortfolio portfolio={props.portfolio}/>
+            <PortfolioCalculator portfolio={name} />
             {/* <button onClick={() => setFormBoolean(!formBoolean)} >Edit Portfolio</button> */}
             {/* {formBoolean? <PortfolioEdit portfolio={id}/>: console.log(formBoolean)} */}
         </div>

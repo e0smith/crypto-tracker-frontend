81033fd11ef9c0fe869da5d9c4ff653bab1194b3
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-30 15:52:41 -0400

    actions/portfolio.js routes changed to work with new users nested route

--- src/actions/portfolio.js
+++ src/actions/portfolio.js
@@ -7,7 +7,7 @@ export const addPortfolio = (portfolioObj) => {
 
 export const createPortfolio = (portfolio) => {    
     return (dispatch) => {
-        fetch("http://localhost:3000/portfolios", {
+        fetch(`http://localhost:3000/users/${localStorage.user}/portfolios`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
@@ -24,7 +24,7 @@ export const createPortfolio = (portfolio) => {
 
 export const getPortfolio = () => {
     return (dispatch) => {
-        fetch("http://localhost:3000/portfolios")
+        fetch(`http://localhost:3000/users/${localStorage.user}/portfolios`)
             .then(resp => resp.json())
             .then(portfolio => {
                 dispatch({
@@ -35,29 +35,29 @@ export const getPortfolio = () => {
         }
 }
 
-export const editPortfolio = (data) => {
-    // debugger;
-    return (dispatch) => {
-      fetch(`http://localhost:3000/portfolios/${data.id}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        method: "PATCH",
-        body: JSON.stringify(data),
-      })
-        .then((response) => response.json())
-        .then((portfolio) => {
-            dispatch({ type: "EDIT_PORTFOLIO", payload: portfolio });
-          }
-        );
-    };
-  };
+// export const editPortfolio = (data) => {
+//     // debugger;
+//     return (dispatch) => {
+//       fetch(`http://localhost:3000/users/${localStorage.user}/portfolios/${data.id}`, {
+//         headers: {
+//           "Content-Type": "application/json",
+//           Accept: "application/json",
+//         },
+//         method: "PATCH",
+//         body: JSON.stringify(data),
+//       })
+//         .then((response) => response.json())
+//         .then((portfolio) => {
+//             dispatch({ type: "EDIT_PORTFOLIO", payload: portfolio });
+//           }
+//         );
+//     };
+//   };
 
 export const deletePortfolio = (Id) => {
     return (dispatch) => {
         dispatch({ type: "DELETE_PORTFOLIO", payload: Id });
-        fetch(`http://localhost:3000/portfolios/${Id}`, {
+        fetch(`http://localhost:3000/users/${localStorage.user}/portfolios/${Id}`, {
         method: "DELETE",
         });
     };

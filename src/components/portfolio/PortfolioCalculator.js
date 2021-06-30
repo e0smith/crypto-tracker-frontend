dc2bd079347646d601096f1b78927f1898e6530e
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-30 15:56:53 -0400

    portfolioCalculator recommit

---
+++ src/components/portfolio/PortfolioCalculator.js
@@ -0,0 +1,21 @@
+import {connect} from 'react-redux'
+
+const PortfolioCalculator = (props) => {
+    let x = "";
+    console.log(props.portfolio.name)
+    const calculate = props.cryptos.cryptos.some(item => props.portfolio.name.toLowerCase() === (x = item).name.toLowerCase()) ? "$" + (x.current_price * props.portfolio.quantity) : "Unknown Currency"
+    
+    return(
+        calculate
+    )
+}
+
+
+const mapStateToProps = (state) => {
+    return{
+      cryptos: state.cryptos,
+      loading: state.loading
+    }
+  }
+
+export default connect(mapStateToProps)(PortfolioCalculator);

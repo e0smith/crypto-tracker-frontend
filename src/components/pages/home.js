6ed19784301fdb80628247fab6c12670c5326e25
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-08-05 15:21:39 -0400

    crypto sorting and code challenge for like buttons created and working in home.js (like button logic commented out)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CryptoCard from '../Card';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-function Home(props){
-  return (
-    <div className="home">
+class Home extends Component {
+
+  handleAlphabeticalSort = () => {
+    this.setState({ cryptos: this.props.cryptos.cryptos.sort((a, b) => a.name > b.name ? 1 : -1)})
+  }
+
+  handlePriceSort = () => {
+    this.setState({ cryptos: this.props.cryptos.cryptos.sort((a, b) => parseFloat(a.current_price) > parseFloat(b.current_price) ? 1 : -1)})
+  }
+
+  // state = { // USED FOR CODE CHALLENGE CAN BE ENABLED
+  //   like: 0
+  // }
+
+  // handleChange = (event) => { // USED FOR CODE CHALLENGE CAN BE ENABLED
+  //   this.setState({like: event.target.value})
+  // }
+
+  render() {
+    return(
+      <div className="home">
       <br />
       <br />
       <br />
+      {/* <input id="like" name="like" type="number" onChange={this.handleChange} value={this.state.like} /> // USED FOR CODE CHALLENGE CAN BE ENABLED */} 
+      <button className='sortCrypto' onClick={this.handleAlphabeticalSort}>Sort by Name</button><br />
+      <button className='sortCrypto' onClick={this.handlePriceSort}>Sort by Price</button><br />
           <div className="cards">
-            {props.cryptos.cryptos.map((crypto) => (<CryptoCard crypto={crypto} />))}
+            {this.props.cryptos.cryptos.map((crypto) => (<CryptoCard crypto={crypto} /* likeInc={this.state.like} USED FOR CODE CHALLENGE CAN BE ENABLED */ />))}
           </div>
-    </div>
-  );
-};
+  </div>)
+  }
+}
 
 const mapStateToProps = (state) => {
   return{
     cryptos: state.cryptos,
   }
 }
-
 export default connect(mapStateToProps)(Home);
 

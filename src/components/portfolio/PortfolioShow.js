75e7bc2588f1f56d55d8c1b6e53e5e7cef62d0dc
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-08-05 15:19:18 -0400

    portfolio update code in PortfolioShow.js

--- src/components/portfolio/PortfolioShow.js
+++ src/components/portfolio/PortfolioShow.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import DeletePortfolio from './PortfolioDelete';
 import PortfolioCalculator from './PortfolioCalculator';
+import { Component } from 'react';
+import { updatePortfolio } from "../../actions/portfolio.js"
+import { connect } from 'react-redux';
 
-function PortfolioShow(props){
-    const { name, quantity} = props.portfolio
-    console.log(props)
-    return(
-        <div className='card'>
-            <p> Name: {name}</p>
-            <p> Quantity: {quantity}</p> 
-            <p> Total Value = {<PortfolioCalculator portfolio={props.portfolio} />}</p>
-            <DeletePortfolio portfolio={props.portfolio}/>
-        </div>
-    )
+class PortfolioShow extends Component {
+
+    state={
+        name: this.props.portfolio.name,
+        quantity: 0 ,
+        user_id: parseInt(localStorage.user)
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.name, event.target.value)
+        this.setState({[event.target.name]: parseFloat(event.target.value)})
+    }
+
+    handleSubmit = () => {
+        this.props.updatePortfolio(this.state, this.props.portfolio.id)
+    }
+
+    render() {
+        const { name, quantity} = this.props.portfolio
+        return(
+                <div className='card'>
+                    <p> Name: {name}</p>
+                    <p> Quantity: {quantity}</p> 
+                    <p> Total Value = {<PortfolioCalculator portfolio={this.props.portfolio} />}</p>
+                    <form onSubmit={this.handleSubmit}>
+                        <label name="quantity">Update Quantity: </label>
+                        <input id="quantity" name="quantity" type="number" step="any" onChange={this.handleChange} value={this.state.quantity} />
+                        <input type="submit" value="Update Quantity" />
+                    </form>
+                    <DeletePortfolio portfolio={this.props.portfolio}/>
+                </div>
+            )
+    }
 }
 
-export default PortfolioShow;
+export default connect(null, {updatePortfolio})(PortfolioShow);
